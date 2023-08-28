1ef6e932490696a0a2da7b65c7a56e2a6a32be5f
JorgeAlfredoMastrandrea <bigjamar@gmail.com>
2023-08-28 13:03:33 -0300

    up_v2

--- JAVASCRIPT/CURSO-JAVASCRIPT-SERGIECODE/16-ARRAYS/10-findIndex.js
+++ JAVASCRIPT/CURSO-JAVASCRIPT-SERGIECODE/16-ARRAYS/10-findIndex.js
@@ -3,9 +3,26 @@
 
 let nombres = []
 nombres = ['Jorge' , 'Alfredo' , 'Edgardo' , 'Daniel' , 'Salvador' , 'Ricardo' ,  ];
-let elNombreQuBusco = 'Ricardo';
+
+let elNombreQuBusco = 'Edgardo';
+
+
 
 const resultado = nombres.findIndex(x = (currentValue , index , arr , thisValue ) =>{
-    
-
+    if (currentValue === elNombreQuBusco) {   
+        console.log(currentValue , ' ', index , ' ' , arr , ' ' , thisValue) // imprimo el nombre que encontró, el índice y el array completo
+        console.log('encontré el nombre que buscaba :  ' , currentValue)        
+    }else{
+        console.log(currentValue , 'no es el que estoy buscando')
+    }
 })
+
+console.log('-------------------------------------------------------------------------------------------------')
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+nombres_2 = ['Jorge' , 'Alfredo' , 'Edgardo' , 'Jorge' , 'Salvador' , 'Alfredo' ,  ];
+const res_2 = nombres_2.findIndex(buscarElPrimero)
+function buscarElPrimero(nombre){
+    return nombre == 'Alfredo';
+}
+console.log(res_2) // va a retornar un indice del primer elemento que cumpla esa condición..
