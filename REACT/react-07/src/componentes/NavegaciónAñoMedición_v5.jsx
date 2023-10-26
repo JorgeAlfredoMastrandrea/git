72daae70790b9c4542f1927c2468aca6795ecdf1
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2023-10-25 21:40:54 -0300

    miercoles_v3

--- REACT/react-07/src/componentes/NavegaciónAñoMedición_v5.jsx
+++ REACT/react-07/src/componentes/NavegaciónAñoMedición_v5.jsx
@@ -16,25 +16,16 @@ import Modal from 'react-bootstrap/Modal';
 import Row from 'react-bootstrap/Row';
 import { BotónAñoAnterior } from './BotónAñoAnterior';
 import { BotónAñoSiguiente } from './BotónAñoSiguiente';
-
-
+import { VisorAño } from './VisorAño';
 
 export const NavegaciónAñoMedición_v5 = (propiedades) => {
 
-    let pointerToObjetoNavLinkAñoMedicion = 0;
-   
-
     const { navLinkAñoMedicion } = propiedades;
     const longitudObjetoNavLinkAñoMedicion = Object.keys(navLinkAñoMedicion).length;
-    pointerToObjetoNavLinkAñoMedicion = longitudObjetoNavLinkAñoMedicion - 1; // la longitud menos uno
 
     const handleClose = () => setShow(false);
     const [isClicked, setIsClicked] = useState(false);
     const [show, setShow] = useState(false);
-
-    let objetoCalendario = {        
-        año: (Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id
-    };
 
     // para manejar el click de la ventana modal
     const handleClicked = (navLinkAñoMedicion) => {
@@ -42,87 +33,35 @@ export const NavegaciónAñoMedición_v5 = (propiedades) => {
         setShow(true);
     }
 
-    /*
-    // let objetoCalendario = [{
-    //     puntero : pointerToObjetoNavLinkAñoMedicion,
-    //     año : (Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id
-    // }];
-    // let año = Object.values(Object.values(objetoCalendario)).at(0).año;
-
-    // let año_ = Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion);
-    // console.log(año_)
-   
-    //console.log((Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id);
-    
-
-    // const [añoOperativo , setAñoOperativo] = useState();
-
-    // para manejar el click del boton año anterior
-    // const [coun_boton_año_Anterior_longitudObjetoNavLinkAñoMedicion, setMensajeDesdeBotonAñoAnterior] = useState(longitudObjetoNavLinkAñoMedicion); // coun_boton_año_siguiente_longitudObjetoNavLinkAñoMedicion es = longitudObjetoNavLinkAñoMedicion
-    // const handleClickBotonAñoAnterior = mensajeBotonAñoAnterior => {
-    //     // 👇️ take the parameter passed from the Child component
-    //     setMensajeDesdeBotonAñoAnterior(mensajeBotonAñoAnterior);
-    //     if ((mensajeBotonAñoAnterior === 'BotonAñoAnteriorPresionado') && (pointerToObjetoNavLinkAñoMedicion > -1)) {            
-    //         pointerToObjetoNavLinkAñoMedicion--;
-    //         if (pointerToObjetoNavLinkAñoMedicion < 0) pointerToObjetoNavLinkAñoMedicion = 0;
-    //         //console.log('argumento desde Boton Año Anterior: ', mensajeBotonAñoAnterior, ' ', 'coun_boton_año_Anterior_longitudObjetoNavLinkAñoMedicion ', coun_boton_año_Anterior_longitudObjetoNavLinkAñoMedicion, ' ', 'pointerToObjetoNavLinkAñoMedicion ', pointerToObjetoNavLinkAñoMedicion);
-    //         objetoCalendario = [
-    //             {
-    //                 puntero : pointerToObjetoNavLinkAñoMedicion,
-    //                 año : (Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id
-    //             }
-    //         ]
-    //         //console.log('---> ' , Object.values(Object.values(objetoCalendario)).at(0).año);
-    //         //año = Object.values(Object.values(objetoCalendario)).at(0).año;
-    //         año = coun_boton_año_Anterior_longitudObjetoNavLinkAñoMedicion 
-    //         console.log('---> ' , año)
-    //         setAñoOperativo({...añoOperativo , año:año});
-    //     }
-    // };
-
-    // para manejar el click del boton año siguiente
-    // const [coun_boton_año_Siguiente_longitudObjetoNavLinkAñoMedicion, setMensajeDesdeBotonAñoSiguiente] = useState(longitudObjetoNavLinkAñoMedicion); // coun_boton_año_Siguiente_longitudObjetoNavLinkAñoMedicion es = longitudObjetoNavLinkAñoMedicion
-    // const handleClickBotonAñoSiguiente = mensajeBotonAñoSiguiente => {
-    //     // 👇️ take the parameter passed from the Child component
-    //     setMensajeDesdeBotonAñoSiguiente(mensajeBotonAñoSiguiente);
-    //     if ((mensajeBotonAñoSiguiente === 'BotonAñoSiguientePresionado') && (pointerToObjetoNavLinkAñoMedicion <= longitudObjetoNavLinkAñoMedicion - 1)) {
-    //         pointerToObjetoNavLinkAñoMedicion++;
-    //         if (pointerToObjetoNavLinkAñoMedicion > longitudObjetoNavLinkAñoMedicion - 1) pointerToObjetoNavLinkAñoMedicion = longitudObjetoNavLinkAñoMedicion - 1;
-    //         //console.log('argumento desde Boton Año Siguiente: ', mensajeBotonAñoSiguiente, ' ', 'coun_boton_año_Siguiente_longitudObjetoNavLinkAñoMedicion ', coun_boton_año_Siguiente_longitudObjetoNavLinkAñoMedicion, ' ', 'pointerToObjetoNavLinkAñoMedicion ', pointerToObjetoNavLinkAñoMedicion);
-    //         objetoCalendario = [
-    //             {
-    //                 puntero : pointerToObjetoNavLinkAñoMedicion,
-    //                 año : (Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id
-    //             }
-    //         ]
-    //         //console.log('---> ' , Object.values(Object.values(objetoCalendario)).at(0).año);
-    //         //año = Object.values(Object.values(objetoCalendario)).at(0).año;
-    //         año = coun_boton_año_Siguiente_longitudObjetoNavLinkAñoMedicion 
-    //         console.log('---> ' , año) 
-    //         setAñoOperativo({...añoOperativo , año:año});
-    //     }
-    // };
-
-    // console.log('---------------> ' , año)
-    */
-
-
-
-    const [añoOperativo, setAñoOperativo] = useState((Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion)).id);
+    const [lrg] = useState(longitudObjetoNavLinkAñoMedicion-1);
+    const [cero] = useState(0);
+    const [ptrAño, setPtrAño] = useState(lrg);
+    const [añoOperativo, setAñoOperativo] = useState(Object.values(navLinkAñoMedicion).at(lrg).id);
 
     const onButtonClickedBotonAñoAnterior = (argumentoQueVieneDelComponenteBotonAnterior) => {
-        //--pointerToObjetoNavLinkAñoMedicion;
-        console.log('-- : ', pointerToObjetoNavLinkAñoMedicion, ' ', argumentoQueVieneDelComponenteBotonAnterior)
-        console.log(Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion));
-        setAñoOperativo((Object.values(navLinkAñoMedicion).at(--pointerToObjetoNavLinkAñoMedicion)).id);
-
+        console.log('B. Anterior -- : ', ptrAño, ' ', argumentoQueVieneDelComponenteBotonAnterior, ' ', Object.values(navLinkAñoMedicion).at(ptrAño))
+        if (Object.values(navLinkAñoMedicion).at(ptrAño) !== 'undefined') {
+            if (ptrAño > cero) {
+                setPtrAño(ptrAño - 1);
+                setAñoOperativo((Object.values(navLinkAñoMedicion).at(ptrAño)).id); console.log('pulse btn ant. : ', ptrAño);
+            } else {
+                setPtrAño(cero);
+                setAñoOperativo((Object.values(navLinkAñoMedicion).at(0)).id);
+            }
+        }
     }
 
     const onButtonClickedBotonAñoSiguiente = (argumentoQueVieneDelComponenteBotonSiguiente) => {
-        ++pointerToObjetoNavLinkAñoMedicion;
-        console.log('-- : ', pointerToObjetoNavLinkAñoMedicion, ' ', argumentoQueVieneDelComponenteBotonSiguiente)
-        console.log(Object.values(navLinkAñoMedicion).at(pointerToObjetoNavLinkAñoMedicion));
-        setAñoOperativo((Object.values(navLinkAñoMedicion).at(++pointerToObjetoNavLinkAñoMedicion)).id);
+        console.log('B. Siguiente -- : ', ptrAño, ' ', argumentoQueVieneDelComponenteBotonSiguiente, ' ', Object.values(navLinkAñoMedicion).at(ptrAño));
+        if (Object.values(navLinkAñoMedicion).at(ptrAño) !== 'undefined') {
+            if (ptrAño < lrg) {
+                setPtrAño(ptrAño + 1);
+                setAñoOperativo((Object.values(navLinkAñoMedicion).at(ptrAño)).id); console.log('pulse btn sig. : ', ptrAño);
+            } else {
+                setPtrAño(lrg); console.log('else --- > pulse btn sig. : ', ptrAño);
+                setAñoOperativo((Object.values(navLinkAñoMedicion).at(lrg)).id);
+            }
+        }
     }
 
 
@@ -136,7 +75,7 @@ export const NavegaciónAñoMedición_v5 = (propiedades) => {
                         <div key={'div1'} className="" >
                             <button key={'div2'} id={'id1'} className="btn-primary btn d-inline-flex align-items-center rounded border-1 collapsed px-0 py-0" data-bs-toggle="collapse" data-bs-target="#" aria-expanded="true"
                                 onClick={(event) => handleClicked(navLinkAñoMedicion)}                            >
-                                <a id="añoBoton" href="#" className="m-1 px-2 py-1  h4 link-body-emphasis d-inline-flex text-decoration-none rounded"><strong>{ }</strong></a>
+                                <a id="añoBoton" href="#" className="m-1 px-2 py-1  h4 link-body-emphasis d-inline-flex text-decoration-none rounded"><strong>{añoOperativo}</strong></a>
                             </button>
                         </div>
 
@@ -164,9 +103,7 @@ export const NavegaciónAñoMedición_v5 = (propiedades) => {
 
                                                     <div className="año">
 
-                                                        <h5 id="año" className="card-title h2 link-body-emphasis d-inline-flex text-decoration-none rounded py-1">
-                                                            <strong>{añoOperativo}</strong>
-                                                        </h5>
+                                                        <VisorAño añoOperativo={añoOperativo} />
 
                                                     </div>
 
@@ -177,6 +114,7 @@ export const NavegaciónAñoMedición_v5 = (propiedades) => {
                                                 <div className="border-top my-2 "></div>
 
                                                 <div className="meses text-center p-0">
+                                                    {console.log('meses')}
 
                                                 </div>
                                             </div>
