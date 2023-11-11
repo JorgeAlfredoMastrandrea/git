f872d511e80c48302fa3ce3e8dd5e15d45c42543
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2023-11-11 12:43:52 -0300

    sabado_v2

--- REACT/react-07_json_componentes_v2/src/componentes/ComponentesGrid/Fila_.jsx
+++ REACT/react-07_json_componentes_v2/src/componentes/ComponentesGrid/Fila_.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Fila_ = ({ src, className, id , mKey, style ,  children }) => {
     return (
-        <div id={id} key={mKey} className={className} style={{style}}>
+        <div id={id} key={mKey} className={className} style={style}>
             {/* Aquí puedes utilizar la propiedad "src" y renderizar otros elementos */}
             {children}
         </div>
