bd40a180a78659af29ce17619985b804d60586c3
JorgeAlfredoMastrandrea <bigjamar@gmail.com>
2024-01-29 12:31:39 -0300

    lunes-29-01-2024

--- FLASK/REACT-FLASK-001/flask-001/app.py
+++ FLASK/REACT-FLASK-001/flask-001/app.py
@@ -8,7 +8,7 @@ CORS(app)  # Habilita CORS para toda la aplicación
 @app.route('/api/ejecutar-funcion', methods=['GET'])
 def ejecutar_funcion():
     # Tu lógica de Python aquí
-    mensage = 'este es un mensage desde python ejecutandose en el servidor con un numero al azar....' + str(random.randint(3, 9))
+    mensage = 'este es un mensage desde python ejecutandose en el servidor con un numero al azar.... ' + str(random.randint(3, 9))
     resultado = {'mensaje': mensage  }
     return jsonify(resultado)
 
