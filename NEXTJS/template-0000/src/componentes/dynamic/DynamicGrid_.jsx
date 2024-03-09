19f4422d63baaaa194155f89f83fb1bc8cc22ffc
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2024-03-08 21:36:03 -0300

    vernesss

--- NEXTJS/template-0000/src/componentes/dynamic/DynamicGrid_.jsx
+++ NEXTJS/template-0000/src/componentes/dynamic/DynamicGrid_.jsx
@@ -1,28 +1,95 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import update from 'immutability-helper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const ROW_TYPE = 'row';
+
+const DraggableRow = ({ index, moveRow, children }) => {
+  const ref = React.useRef(null);
+  const [, drop] = useDrop({
+    accept: ROW_TYPE,
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      // Mueve la fila solo cuando el cursor pasa la mitad de la fila objetivo
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      moveRow(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ROW_TYPE,
+    item: () => ({ index }),
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div
+      ref={ref}
+      className="row align-items-center"
+      style={{
+        background: isDragging ? 'yellow' : 'transparent', // Cambia el color de fondo cuando se arrastra
+        cursor: 'grab'
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+
 export const DynamicGrid_ = () => {
+
   const [rows, setRows] = useState([{ columns: [{ id: Date.now(), width: 4, color: 'light' }] }]);
+  const [hoveredColumn, setHoveredColumn] = useState({ rowIndex: null, columnIndex: null });
+  const [draggedRowIndex, setDraggedRowIndex] = useState(null);
+  const [editingRowIndex, setEditingRowIndex] = useState(null);
 
-  // Función para manejar el fin del arrastre
-  const onDragEnd = (result) => {
-    const { destination, source } = result;
 
-    // Si no hay destino (se soltó fuera de cualquier droppable), no hacemos nada
-    if (!destination) {
-      return;
-    }
+  // // Antes de la función return en tu componente
+  // const handleDragStart = (e, index) => {
+  //   setDraggedRowIndex(index);
+  //   // Aquí puedes añadir más lógica si es necesario
+  // };
 
-    // Reordenar las filas
-    const newRows = Array.from(rows);
-    const [removed] = newRows.splice(source.index, 1);
-    newRows.splice(destination.index, 0, removed);
+  // const handleDragEnd = (e) => {
+  //   setDraggedRowIndex(null);
+  //   // Aquí puedes añadir más lógica si es necesario
+  // };
 
-    setRows(newRows);
-  };
-  
+
+  const moveRow = useCallback((dragIndex, hoverIndex) => {
+    const dragRow = rows[dragIndex];
+    setRows(update(rows, {
+      $splice: [
+        [dragIndex, 1],
+        [hoverIndex, 0, dragRow],
+      ],
+    }));
+  }, [rows]);
+
+
   const addRow = () => {
     setRows([...rows, { columns: [{ id: Date.now(), width: 4, color: 'light' }] }]);
   };
@@ -71,12 +138,7 @@ export const DynamicGrid_ = () => {
     }
   };
 
-  const changeColor = (rowIndex, columnIndex) => {
-    const newRows = [...rows];
-    const column = newRows[rowIndex].columns[columnIndex];
-    column.color = column.color === 'light' ? 'info' : 'light';
-    setRows(newRows);
-  };
+
 
   const exportToHTML = () => {
     let html = `<!DOCTYPE html>
@@ -94,7 +156,10 @@ export const DynamicGrid_ = () => {
     rows.forEach((row, rowIndex) => {
       html += `  <div class="row">\n`;
       row.columns.forEach((column, columnIndex) => {
-        html += `    <div class="col-${column.width} p-2 border text-center bg-${column.color}">\n`;
+        // Asume que el ancho de columna es el mismo para todos los tamaños de pantalla por simplicidad
+        // Modificar aquí según necesidades específicas de diseño responsive
+        const responsiveWidth = `col-12 col-sm-${column.width} col-md-${column.width} col-lg-${column.width} col-xl-${column.width}`;
+        html += `    <div class="${responsiveWidth} p-2 border text-center bg-${column.color}">\n`;
         html += `      Fila ${rowIndex + 1}, Columna ${columnIndex + 1} - Ancho: ${column.width}\n`;
         html += `    </div>\n`;
       });
@@ -123,50 +188,57 @@ export const DynamicGrid_ = () => {
     }, 0);
   };
 
+
   return (
-    <div className="container mt-3">
-      <button onClick={addRow} className="btn btn-primary mb-1">+R</button>
-      <button onClick={exportToHTML} className="btn btn-success mb-1">Exportar a HTML</button>
-      {rows.map((row, rowIndex) => (
-        <div key={rowIndex} className="mb-0">
-          <div className="row align-items-center" style={{ marginBottom: '5px' }}>
-            {row.columns.map((column, columnIndex) => (
-              <div key={column.id} className={`col-${column.width} border text-center bg-${column.color} p-2 position-relative`} style={{ padding: '.25rem .4rem', fontSize: '.75rem', lineHeight: '1', borderRadius: '.2rem' }}>
-                <div className="btn-group mt-1">
-                  <button onClick={() => adjustColumnWidth(rowIndex, columnIndex, -1)} className="btn btn-secondary"
-                    style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>
-                    -
-                  </button>
-                  <button onClick={() => adjustColumnWidth(rowIndex, columnIndex, 1)} className="btn btn-secondary btn-xs"
-                    style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>
-                    +
-                  </button>
-                  <span style={{ fontSize: "0.75rem", padding: "0 0.4rem" }}>{column.width}</span>
-                  <button onClick={() => deleteColumn(rowIndex, columnIndex)} className="btn btn-danger btn-xs"
-                    style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>
-                    X
-                  </button>
-                  {columnIndex === row.columns.length - 1 && (
-                    <>
-                      <button onClick={() => deleteRow(rowIndex)} className="btn btn-danger btn-xs"
-                        style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>
-                        -R
-                      </button>
-                      <button onClick={() => addRowInBetween(rowIndex)} className="btn btn-info btn-xs"
-                        style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>
-                        +R
-                      </button>
-                    </>
-                  )}
-                </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="container mt-3">
+        <button onClick={addRow} className="btn btn-primary mb-1">+R</button>
+        <button onClick={exportToHTML} className="btn btn-success mb-1">Exportar a HTML</button>
+        {rows.map((row, rowIndex) => (
+          <DraggableRow
+            key={rowIndex}
+            index={rowIndex}
+            moveRow={moveRow}
+            isEditing={editingRowIndex === rowIndex}
+            setEditingRowIndex={setEditingRowIndex}
+          >
+            <div key={rowIndex} className="mb-0">
+
+              <div
+                className={`row align-items-center ${draggedRowIndex === rowIndex ? 'dragged-row' : ''}`}
+                style={{ marginBottom: '5px' }}
+              >
+                {row.columns.map((column, columnIndex) => (
+                  <div
+                    key={column.id}
+                    className={`col-${column.width} border text-center bg-${column.color} p-2 position-relative`}
+                    style={{ padding: '.25rem .4rem', fontSize: '.75rem', lineHeight: '1', borderRadius: '.2rem' }}
+                    onMouseEnter={() => setHoveredColumn({ rowIndex, columnIndex })}
+                    onMouseLeave={() => setHoveredColumn({ rowIndex: null, columnIndex: null })}
+                  >
+                    {hoveredColumn.rowIndex === rowIndex && hoveredColumn.columnIndex === columnIndex && (
+                      <div className="btn-group mt-1">
+                        <button onClick={() => adjustColumnWidth(rowIndex, columnIndex, -1)} className="btn btn-secondary btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>-</button>
+                        <button onClick={() => adjustColumnWidth(rowIndex, columnIndex, 1)} className="btn btn-secondary btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>+</button>
+                        <button onClick={() => deleteColumn(rowIndex, columnIndex)} className="btn btn-danger btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>X</button>
+                        {columnIndex === row.columns.length - 1 && (
+                          <>
+                            <button onClick={() => deleteRow(rowIndex)} className="btn btn-danger btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>-R</button>
+                            <button onClick={() => addRowInBetween(rowIndex)} className="btn btn-info btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>+R</button>
+                          </>
+                        )}
+                        {row.columns.reduce((acc, column) => acc + column.width, 0) < 12 && columnIndex === row.columns.length - 1 && (
+                          <button onClick={() => addColumn(rowIndex)} className="btn btn-primary btn-xs" style={{ padding: "0.5rem 0.4rem", fontSize: "0.5rem", lineHeight: "0.5", borderRadius: "0.1rem" }}>+C</button>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                ))}
               </div>
-            ))}
-            {row.columns.reduce((acc, column) => acc + column.width, 0) < 12 && (
-              <button onClick={() => addColumn(rowIndex)} className="btn btn-primary btn-sm mt-2" style={{ padding: '.25rem .4rem', fontSize: '.75rem', lineHeight: '1', borderRadius: '.2rem' }}>+C</button>
-            )}
-          </div>
-        </div>
-      ))}
-    </div>
-  );  
+            </div>
+          </DraggableRow>
+        ))}
+      </div>
+    </DndProvider>
+  );
 };
